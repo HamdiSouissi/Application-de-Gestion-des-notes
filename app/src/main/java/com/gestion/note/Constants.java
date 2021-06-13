8ca22f1cae56312ebe3c44686c28a199aefc24a7
HamdiSouissi <85841080+HamdiSouissi@users.noreply.github.com>
2021-06-13 18:53:09 +0100

    first

---
+++ app/src/main/java/com/gestion/note/Constants.java
@@ -0,0 +1,8 @@
+package com.gestion.note;
+
+public interface Constants {
+    String id="id";
+    String note_text="note_text";
+    String create_time="create_time";
+}
+
---
+++ app/src/main/java/com/gestion/note/NoteClickListener.java
@@ -0,0 +1,5 @@
+package com.gestion.note;
+
+public interface NoteClickListener {
+    void onClickItem(NoteModel noteModel);
+}
